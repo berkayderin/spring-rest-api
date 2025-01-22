cbc4a3d9f5a4360dfba7d7da42eedfff2a49ce63
Berkay Derin <zewodi@gmail.com>
2025-01-22 14:50:47 +0300

    chore: add component scanning for base package in SpringBootRestApiApplication

--- src/main/java/com/berkayderin/starter/SpringBootRestApiApplication.java
+++ src/main/java/com/berkayderin/starter/SpringBootRestApiApplication.java
@@ -2,8 +2,10 @@ package com.berkayderin.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.berkayderin")
 public class SpringBootRestApiApplication {
 
     public static void main(String[] args) {
